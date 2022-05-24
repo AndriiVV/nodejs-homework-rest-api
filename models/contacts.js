36af9e6c8a4b1a140cd99705628d74efbc04b98d
Andrii Vinnykov <andrey.vinnikov@gmail.com>
2022-05-24 23:46:17 +0300

    get functions

--- models/contacts.js
+++ models/contacts.js
@@ -1,12 +1,13 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-const contactsPath = path.join(__dirname, "./models/contacts.json");
+const contactsPath = path.join(__dirname, "./contacts.json");
 
 const listContacts = async () => {
 	try {
 		const data = await fs.readFile(contactsPath);
 		const result = JSON.parse(data);
+		// console.log(result);
 		return result;
 	} catch (err) {
 		err.message = "listContacts error";
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,32 @@
-const express = require('express')
+const express = require("express");
+const { listContacts, getContactById } = require("../../models/contacts");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+	const contacts = await listContacts();
+	res.status(200).send(contacts);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", async (req, res, next) => {
+	const contact = await getContactById(req.params.id);
+	if (!contact.length) {
+		res.status(404).send("Not found");
+	} else {
+		res.status(200).send(contact);
+	}
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+	res.json({ message: "add contact" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+	res.json({ message: "remove contact" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+	res.json({ message: "update contact" });
+});
 
-module.exports = router
+module.exports = router;
